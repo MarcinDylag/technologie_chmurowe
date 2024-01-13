2e8da3f57f132efd60a0feaa9699d32e2366bd43
MarcinD <dylag.marcin@gmail.com>
2024-01-13 15:57:16 +0100

    adding picture and python file

---
+++ main.py
@@ -0,0 +1,41 @@
+import cv2
+import imutils
+
+# Initializing the HOG person
+# detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
+img = cv2.imread('peron.png')
+img = cv2.resize(img, (1200,600))
+
+#print(type(img))
+#print(img.shape)
+
+#cv2.imshow('image', img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+# Resizing the Image
+#image = imutils.resize(image,width=min(400, image.shape[1]))
+
+# Detecting all the regions in the
+# Image that has a pedestrians inside it
+(rects, weights) = hog.detectMultiScale(img,
+                                    winStride=(4, 4),
+                                    padding=(4, 4),
+                                    scale=1.05)
+
+# Drawing the regions in the Image
+for (x, y, w, h) in rects:
+    cv2.rectangle(img, (x, y),
+                  (x + w, y + h),
+                  (0, 0, 255), 2)
+
+# Showing the output Image
+
+print(f'Found {len(rects)} humans')
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
