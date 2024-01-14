8a4bc9e6cf65f3b83c9b1a04080c6f9a86019458
Student <student@it.uekat.local>
2024-01-14 11:51:52 +0100

    formatting changes in main.py

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
 import cv2
-#API
+# API
 from flask import Flask
 from flask_restful import Resource, Api
 # Initializing the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-#API
+# API
 app = Flask(__name__)
 api = Api(app)
+
 
 class PeopleCounter(Resource):
     def get(self):
         # Loading and Resizing the Image
         img = cv2.imread('peron2.jpg')
-        img = cv2.resize(img, (800,500))
+        img = cv2.resize(img, (800, 500))
         # Detecting all the regions in the image with people in it
         (rects, weights) = hog.detectMultiScale(img, winStride=(3, 3), padding=(4, 4), scale=1.05)
 
         return {'people count': len(rects)}
 
-api.add_resource(PeopleCounter, '/')
 
+api.add_resource(PeopleCounter, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-
-
-
-#for (x, y, w, h) in rects:
-#cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+# for (x, y, w, h) in rects:
+# cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
 # Showing the output Image
-#print(f'Found {len(rects)} humans')
-#cv2.imshow("Image", img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+# print(f'Found {len(rects)} humans')
+# cv2.imshow("Image", img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
