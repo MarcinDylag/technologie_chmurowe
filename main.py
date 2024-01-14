3ec88b45956f1edd8109e51e6b5f5819786d75ae
Student <student@it.uekat.local>
2024-01-14 15:48:10 +0100

    main file modification - dynamic GET added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
+import urllib.request
 # API
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 # Initializing the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
@@ -9,9 +10,10 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 app = Flask(__name__)
 api = Api(app)
 
-url = 'https://vs-static.virtualspeech.com/img/env/lecture-hall-full.jpg'
+name = 'download.jpg'
 
-class PeopleCounter(Resource):
+
+class PeopleCounterStatic(Resource):
     def get(self):
         # Loading and Resizing the Image
         img = cv2.imread('peron2.jpg')
@@ -19,19 +21,22 @@ class PeopleCounter(Resource):
         # Detecting all the regions in the image with people in it
         (rects, weights) = hog.detectMultiScale(img, winStride=(3, 3), padding=(4, 4), scale=1.05)
 
-        return {'people count': len(rects)}
+        return {'peopleCount': len(rects)}
 
 
-api.add_resource(PeopleCounter, '/')
+class PeopleCounterDynamic(Resource):
+
+    def get(self):
+        url = request.args.get('url')
+        urllib.request.urlretrieve(url, name)
+        img2 = cv2.imread(name)
+        img2 = cv2.resize(img2, (800, 500))
+        (rects, weights) = hog.detectMultiScale(img2, winStride=(3, 3), padding=(4, 4), scale=1.05)
+        return {'peopleCount': len(rects)}
+
+
+api.add_resource(PeopleCounterStatic, '/')
+api.add_resource(PeopleCounterDynamic, '/dynamic')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# for (x, y, w, h) in rects:
-# cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-# Showing the output Image
-# print(f'Found {len(rects)} humans')
-# cv2.imshow("Image", img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
