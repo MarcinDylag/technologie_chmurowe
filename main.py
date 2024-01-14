7790fab5fd605e3d7dc4ba64ef51e8ec41eac6bc
Student <student@it.uekat.local>
2024-01-14 10:36:44 +0100

    create API

--- main.py
+++ main.py
@@ -1,41 +1,39 @@
 import cv2
-import imutils
-
-# Initializing the HOG person
-# detector
+#API
+from flask import Flask
+from flask_restful import Resource, Api
+# Initializing the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+#API
+app = Flask(__name__)
+api = Api(app)
+
+class PeopleCounter(Resource):
+    def get(self):
+        # Loading and Resizing the Image
+        img = cv2.imread('peron2.jpg')
+        img = cv2.resize(img, (800,500))
+        # Detecting all the regions in the image with people in it
+        (rects, weights) = hog.detectMultiScale(img, winStride=(3, 3), padding=(4, 4), scale=1.05)
+
+        return {'people count': len(rects)}
+
+api.add_resource(PeopleCounter, '/')
 
 
-img = cv2.imread('peron2.jpg')
-img = cv2.resize(img, (800,500))
+if __name__ == '__main__':
+    app.run(debug=True)
 
-#print(type(img))
-#print(img.shape)
 
-#cv2.imshow('image', img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-# Resizing the Image
-#image = imutils.resize(image,width=min(400, image.shape[1]))
 
-# Detecting all the regions in the
-# Image that has a pedestrians inside it
-(rects, weights) = hog.detectMultiScale(img,
-                                    winStride=(4, 4),
-                                    padding=(4, 4),
-                                    scale=1.05)
 
-# Drawing the regions in the Image
-for (x, y, w, h) in rects:
-    cv2.rectangle(img, (x, y),
-                  (x + w, y + h),
-                  (0, 0, 255), 2)
+
+#for (x, y, w, h) in rects:
+#cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
 # Showing the output Image
-
-print(f'Found {len(rects)} humans')
-cv2.imshow("Image", img)
-cv2.waitKey(0)
-
-cv2.destroyAllWindows()
+#print(f'Found {len(rects)} humans')
+#cv2.imshow("Image", img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
