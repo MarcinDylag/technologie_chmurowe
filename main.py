abf048b8d08707801bc8bc424288e4531d70ef4f
Student <student@it.uekat.local>
2024-01-14 13:33:33 +0100

    add actions.yml

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 app = Flask(__name__)
 api = Api(app)
 
-
+url = 'https://vs-static.virtualspeech.com/img/env/lecture-hall-full.jpg'
 class PeopleCounter(Resource):
     def get(self):
         # Loading and Resizing the Image
