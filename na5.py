92c07b30029168a23efafb527d497329c647922c
Marcin Dylag <dylag.marcin@gmail.com>
2024-02-01 00:41:24 +0100

    fixed PEP8 coding

--- na5.py
+++ na5.py
@@ -4,6 +4,7 @@ import cv2
 import urllib.request
 import base64
 
+
 def encode_img(img, im_type):
     """Encodes an image as a png and encodes to base 64 for display."""
     success, encoded_img = cv2.imencode('.{}'.format(im_type), img)
@@ -11,14 +12,17 @@ def encode_img(img, im_type):
         return base64.b64encode(encoded_img).decode()
     return ''
 
+
 # Initializing the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 app = Flask(__name__)
 
+
 # Declare img as a global variable
 img = None
+
 
 @app.route("/", methods=['GET'])
 def home():
@@ -26,6 +30,7 @@ def home():
     img = 'https://thumbs.dreamstime.com/z/ludzie-na-dworcu-kolejowym-lotnisku-czekaj%C4%85cy-poci%C4%85g-frankfurt-niemcy-maj-259290207.jpg'
     if request.method == "GET":
         return render_template("index.html", image=img)
+
 
 @app.route("/recognize")
 def recognize():
@@ -50,6 +55,7 @@ def recognize():
 
         return render_template("recognize.html", image=img_src, people=peopleCount)
 
+
 @app.route("/change", methods=["POST", "GET"])
 def change():
     global img
@@ -59,11 +65,13 @@ def change():
     else:
         return render_template("change.html"), img
 
+
 @app.route("/newimage/<path:url>")
 def newimage(url):
     global img
     img = url
     return render_template("index.html", image=img)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
